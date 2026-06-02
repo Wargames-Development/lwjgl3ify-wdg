ac5b982f4d07654aaa0e2d8394d55eeca5114b36
Raven Szewczyk <git@eigenraven.me>
2026-06-02 18:14:13 +0100

    Update lwjgl3 + SDL, revert x11 scroll bug workaround

--- src/main/java/org/lwjglx/Lwjgl3ifyEventLoop.java
+++ src/main/java/org/lwjglx/Lwjgl3ifyEventLoop.java
@@ -46,13 +46,10 @@ public class Lwjgl3ifyEventLoop {
     public static final SDL_MouseButtonEvent mouseButtonEvent = event.button();
     public static final SDL_MouseWheelEvent mouseWheelEvent = event.wheel();
 
-    public static final boolean isMouseWheelBugged = "x11".equals(SDL_GetCurrentVideoDriver());
-
     private static void internalPumpEvents() {
         if (!SDL_IsMainThread()) {
             throw new IllegalStateException("SDL Event pump called from a non-main thread " + Thread.currentThread());
         }
-        int lastWheelID = -1;
         SDL_PumpEvents();
         int peepedEvents = 0;
         while ((peepedEvents = SDL_PeepEvents(eventPeepArray, SDL_GETEVENT, SDL_EVENT_FIRST, SDL_EVENT_LAST)) > 0) {
@@ -90,15 +87,6 @@ public class Lwjgl3ifyEventLoop {
                             event.type() == SDL_EVENT_MOUSE_BUTTON_DOWN);
                     }
                     case SDL_EVENT_MOUSE_WHEEL -> {
-                        // duplicate scroll wheel inputs under certain circumstances, ignored when device ID mismatch
-                        if (isMouseWheelBugged) {
-                            if (lastWheelID != -1 && lastWheelID != mouseWheelEvent.which()) {
-                                lastWheelID = -1;
-                                continue;
-                            }
-                            lastWheelID = mouseWheelEvent.which();
-                        }
-
                         float xoffset = mouseWheelEvent.x();
                         float yoffset = mouseWheelEvent.y();
 
